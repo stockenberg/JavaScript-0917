804ce662ce5af49b12c0d266bad5ceec3907be46
Marten Stockenberg <MStockenberg@googlemail.com>
2018-01-10 12:25:41 +0100

    end lesson 2

--- js/script.js
+++ js/script.js
@@ -1,29 +1,17 @@
+var firstname = "Marten",
+    gender = "female",
+    lastname = "Stockenberg",
+    salutation;
 
-// Erstelle ein variable mit dem Wert "Hallo"
-// Gebe diese mit dem Befehl console.log() aus
-// Erstelle eine Kontrollstruktur die den Wert der Variable prüft
 
-/*
-    if()
-    {
-
+function nameWithSalutation(firstname, gender, lastname) {
+    if (gender == "male") {
+        return "Sehr geehrter Herr " + firstname;
+    } else if (gender == "female"){
+        return "Sehr geehrte Frau " + firstname;
     }
-    else
-    {
+}
 
-    }
+salutation = nameWithSalutation(firstname, gender, lastname);
 
-    for(var i = 0; i < 12; i++){
-        console.log(i);
-    }
-
-    window.prompt();
-    window.alert();
-    window.location.href = URL
-    window.history.back()
-    window.history.next()
-    console.error();
-    console.dir();
-    console.warn();
-
- */
+console.log(salutation);
