adf1161133feb4aed2b10da2483864fd45c80784
Marten Stockenberg <MStockenberg@googlemail.com>
2018-01-17 11:23:01 +0100

    init

--- js/script.js
+++ js/script.js
@@ -5,32 +5,6 @@
     TODO: JavaScript Lightbox
     TODO: Tabs with JavaScript
 */
-/*
-var textClass,
-    headline,
-    paragraph;
-
-var text = ["wie", "gehts!"];
-
-textClass = document.getElementsByClassName("text");
-headline = document.getElementById('headline');
-paragraph = document.getElementsByTagName('div');
-
-console.log(paragraph);
-
-textClass[0].innerHTML = "<strong>" + textClass[0].innerText + "</strong>";
-textClass[2].innerText = text[0];
-textClass[3].innerText = text[1];
-
-console.log(textClass);*/
-
-var container = document.getElementById('main');
-console.log(container);
-
-var p = container.children[1];
-
-console.log(p.children[0]);
-
 
 
 
