3c64e3e3f8dc439485502202d9f9b1bf2974515e
Marten Stockenberg <MStockenberg@googlemail.com>
2018-01-17 11:22:45 +0100

    init

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
-var firstname = "Marten",
-    gender = "female",
-    lastname = "Stockenberg",
-    salutation;
+/*
+    TODO: JavaScript Navigation-Toggle
+    TODO: JavaScript Todo-List
+    TODO: JavaScript Image Slider
+    TODO: JavaScript Lightbox
+    TODO: Tabs with JavaScript
+*/
+/*
+var textClass,
+    headline,
+    paragraph;
+
+var text = ["wie", "gehts!"];
+
+textClass = document.getElementsByClassName("text");
+headline = document.getElementById('headline');
+paragraph = document.getElementsByTagName('div');
+
+console.log(paragraph);
+
+textClass[0].innerHTML = "<strong>" + textClass[0].innerText + "</strong>";
+textClass[2].innerText = text[0];
+textClass[3].innerText = text[1];
+
+console.log(textClass);*/
+
+var container = document.getElementById('main');
+console.log(container);
+
+var p = container.children[1];
+
+console.log(p.children[0]);
 
 
-function nameWithSalutation(firstname, gender, lastname) {
-    if (gender == "male") {
-        return "Sehr geehrter Herr " + firstname;
-    } else if (gender == "female"){
-        return "Sehr geehrte Frau " + firstname;
-    }
-}
 
-salutation = nameWithSalutation(firstname, gender, lastname);
 
-console.log(salutation);
