546dfea100ac5fc32f286579216b37b845a0b8f1
Marten Stockenberg <MStockenberg@googlemail.com>
2018-01-18 12:15:54 +0100

    deleted function

--- js/script.js
+++ js/script.js
@@ -4,14 +4,4 @@ var firstname = "Marten",
     salutation;
 
 
-function nameWithSalutation(firstname, gender, lastname) {
-    if (gender == "male") {
-        return "Sehr geehrter Herr " + firstname;
-    } else if (gender == "female"){
-        return "Sehr geehrte Frau " + firstname;
-    }
-}
-
-salutation = nameWithSalutation(firstname, gender, lastname);
-
 console.log(salutation);
