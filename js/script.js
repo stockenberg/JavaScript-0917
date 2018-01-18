6082349363507cd40c43ef1db0efc255694d8c37
Marten Stockenberg <MStockenberg@googlemail.com>
2018-01-18 11:39:48 +0100

    lightbox finished - git basics

--- js/script.js
+++ js/script.js
@@ -1,20 +1,54 @@
 /*
+    Beispiele
     TODO: JavaScript Navigation-Toggle
     TODO: JavaScript Todo-List
     TODO: JavaScript Image Slider
-    TODO: JavaScript Lightbox
+    TODO: JavaScript Lightbox - in action
     TODO: Tabs with JavaScript
+
+    Einfacher und Besser zusammen arbeiten
+    TODO: Git Install
+    TODO: branches
+    TODO: Git pull
+    TODO: Git Checkout
+
 */
 
 
-var img = document.getElementsByClassName('openImg');
+var imgContainer = document.getElementById('gallery');
+var listItems = imgContainer.children[0].children;
 
-console.log(img);
+for (var index = 0; index < listItems.length; index++) {
+    listItems[index].children[0].onclick = function (event) {
 
-for(var index = 0; index < img.length; index++){
-    img[index].onclick = function (event) {
         var srcCopy = event.target.src;
-        var lightbox = document.getElementsByClassName("lightbox")[0].src = srcCopy;
+
+        /** Delete lightbox elements if they are, no duplicates **/
+        if (document.getElementsByClassName('lightbox').length > 0) {
+            document.getElementsByClassName('lightbox')[0].remove();
+        }
+
+        /* Create IMG tag after clickEvent */
+        var overlay = document.createElement('div');
+        overlay.id = 'overlay';
+
+        var element = document.createElement('img');
+        element.src = srcCopy;
+        element.classList = 'lightbox';
+
+        overlay.appendChild(element);
+        document.body.appendChild(overlay);
+
+
+        /** Close Lightbox **/
+        document.getElementById('overlay').onclick = function (event) {
+            console.log('huhu');
+
+            if(event.target.tagName !== "IMG"){
+                document.getElementById('overlay').remove();
+            }
+        };
+
     };
 }
 
