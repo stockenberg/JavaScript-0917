3522d0e36b1ccba5cef607f5c2c9a982f4006322
Marten Stockenberg <MStockenberg@googlemail.com>
2018-01-25 11:35:46 +0100

    add to cart and clear cart ready

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 
+var cart = [];
 
+function addToCart(price, name, amount){
+
+    if(localStorage.cart){
+       cart = JSON.parse(localStorage.cart);
+    }
+    cart.push({price: price, name: name, amount: amount});
+    localStorage.cart = JSON.stringify(cart);
+}
+
+function clearCart() {
+    localStorage.removeItem('cart');
+}
+
+function debug() {
+    console.log('cart -------------------------------')
+    console.log(cart);
+    console.log('------------------------------------')
+
+    console.log('localStorage------------------------');
+    console.log(localStorage);
+    console.log('------------------------------------')
+}
