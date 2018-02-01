8ca10832d4b0e9e5afd175d0acfa35e833c7a168
Marten Stockenberg <MStockenberg@googlemail.com>
2018-02-01 10:42:21 +0100

    step 1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,35 @@
 
 
+/*
+    Datentypen
+    Objekte vs Arrays
+    Schleifen
+    Funktionen
+        + Parameter
+        + Geltungsbereiche
+    Kontrollstrukturen
+        + IF
+        + Switch
+    DOM
+    Browser Object Model
+    Traversing / Navigation im Dom
+    Die Arbeit mit der Konsole
+    Events
+    Wichtige Objekte
+        + Navigator
+        + Window
+        + Screen
+        + Console
+        + History
+        + Location
+    Try / Catch
+ */
+
+var posts = document.getElementsByClassName('posts');
+
+for (var i = 0; i < posts.length; i++){
+    posts[i].onmouseover = function (event) {
+        console.log(event);
+        event.target.style.background = "red";
+    }
+}
